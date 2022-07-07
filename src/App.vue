de9645c24d0d3a97e0cf26955d424a01c9d4fa6b
Florent Beauchamp <flo850@free.fr>
2022-07-07 09:10:31 +0200

    css

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,83 @@
         <InfraPoolList />
       </nav>
       <main class="main">
-        <RouterView />
+        <form class="form" style="width:40rem; margin: 0 auto">
+
+          <label class="form-control ">
+            <span>Aimez-vous les petits pois ?</span>
+            <FontAwesomeIcon :icon="faServer" class="prepend" />
+            <input type="text" name="petits_pois">
+            <FontAwesomeIcon :icon="faPlay" class="append" />
+          </label>
+
+
+          <label class="form-control ">
+            <span>Aimez-vous les petits pois ?</span>
+            <input type="text" name="petits_pois">
+            <FontAwesomeIcon :icon="faMoon" class="append" />
+          </label>
+
+          <label class="form-control ">
+            <span>Aimez-vous les petits pois ?</span>
+            <FontAwesomeIcon :icon="faDisplay" class="prepend" />
+            <input type="text" name="petits_pois">
+          </label>
+          <label class="form-control ">
+            <span>Aimez-vous les petits pois ?</span>
+            <input type="text" name="petits_pois">
+          </label>
+
+          <label class="form-control inline">
+            <input type="checkbox" name="checkbox" />
+            <span>Checkbox</span>
+          </label>
+
+          <label class="form-control inline">
+            <span>Checkbox</span>
+            <input type="checkbox" name="checkbox" />
+          </label>
+
+          <label class="form-control inline">
+            <input type="checkbox" name="checkbox-checked" checked />
+            <span>Checkbox checked </span>
+          </label>
+
+          <label class="form-control">
+            <span>Aimez-vous les petits pois ?</span>
+            <select>
+              <option value="1">un</option>
+              <option value="2">deux</option>
+              <option value="3">trois</option>
+              <option value="4">quatre</option>
+
+            </select>
+          </label>
+          <label class="form-control inline">
+            <span>Combien de petits pois ?</span>
+            <select>
+              <option value="1">un</option>
+              <option value="2">deux</option>
+              <option value="3">trois</option>
+              <option value="4">quatre</option>
+
+            </select>
+          </label>
+
+          <label class="form-control inline">
+            <span>Aimez-vous les petits pois en ligne </span>
+            <input type="text" name="petits_pois">
+            <FontAwesomeIcon :icon="faMoon" class="append" />
+          </label>
+          <label class="form-control">
+            <span>Décrivez votre amour des petits pois </span>
+            <textarea></textarea>
+          </label>
+
+          <input type="submit" value="submit " class="form-control" />
+          <button class="form-control">button</button>
+          <button class="form-control inverse">button</button>
+
+        </form>
       </main>
     </div>
   </div>
@@ -17,6 +93,7 @@
   import AppHeader from '@/components/AppHeader.vue';
   import InfraPoolList from '@/components/infra/InfraPoolList.vue';
   import { useXenApiStore } from '@/stores/xen-api.store';
+  import { faDisplay,faMoon, faPlay,  faServer } from '@fortawesome/free-solid-svg-icons';
 
   const xenApiStore = useXenApiStore();
   xenApiStore.init();
@@ -25,6 +102,7 @@
 
 <style lang="postcss">
   @import '@/assets/base.css';
+  @import '@/assets/form.postcss';
 
   .nav {
     overflow: auto;

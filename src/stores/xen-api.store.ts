0da901bd64b0a3072ae710efceb2d83dbae2506d
Thierry Goettelmann <thierry@byscripts.info>
2022-07-07 10:27:12 +0200

    Stay logged in

--- src/stores/xen-api.store.ts
+++ src/stores/xen-api.store.ts
@@ -1,25 +1,27 @@
-import type { XenApiRecord } from '@/libs/xen-api';
-import XenApi from '@/libs/xen-api';
-import { useConsoleStore } from '@/stores/console.store';
-import { useHostMetricsStore } from '@/stores/host-metrics.store';
-import { useHostStore } from '@/stores/host.store';
-import { usePoolStore } from '@/stores/pool.store';
-import { useVmGuestMetricsStore } from '@/stores/vm-guest-metrics.store';
-import { useVmMetricsStore } from '@/stores/vm-metrics.store';
-import { useVmStore } from '@/stores/vm.store';
-import { useRecordsStore } from '@/stores/records.store';
-import { defineStore } from 'pinia';
-import { ref, watchEffect } from 'vue';
+import type { XenApiRecord } from "@/libs/xen-api";
+import XenApi from "@/libs/xen-api";
+import { useConsoleStore } from "@/stores/console.store";
+import { useHostMetricsStore } from "@/stores/host-metrics.store";
+import { useHostStore } from "@/stores/host.store";
+import { usePoolStore } from "@/stores/pool.store";
+import { useVmGuestMetricsStore } from "@/stores/vm-guest-metrics.store";
+import { useVmMetricsStore } from "@/stores/vm-metrics.store";
+import { useVmStore } from "@/stores/vm.store";
+import { useRecordsStore } from "@/stores/records.store";
+import { useLocalStorage } from "@vueuse/core";
+import { defineStore } from "pinia";
+import { computed, ref, watch, watchEffect } from "vue";
 
-export const useXenApiStore = defineStore('xen-api', () => {
+export const useXenApiStore = defineStore("xen-api", () => {
   const xenApi = new XenApi(import.meta.env.VITE_XO_HOST);
-  const currentSessionId = ref();
-  const isConnected = ref(false)
-  const isConnecting = ref(false) 
+  const currentSessionId = useLocalStorage<string | null>("sessionId", null);
+  xenApi.sessionId = currentSessionId.value ?? undefined;
+  const isConnected = computed(() => currentSessionId.value !== null);
+  const isConnecting = ref(false);
 
   async function getXapi() {
     if (!currentSessionId.value) {
-      throw new Error('Not connected to xapi')
+      throw new Error("Not connected to xapi");
     }
 
     return xenApi;
@@ -41,10 +43,14 @@ export const useXenApiStore = defineStore('xen-api', () => {
       xapi.registerWatchCallBack((results) => {
         const recordsStore = useRecordsStore();
         results.forEach((result) => {
-          if (result.operation === 'del') {
+          if (result.operation === "del") {
             recordsStore.removeRecord(result.class, result.ref);
           } else {
-            recordsStore.addOrReplaceRecord(result.class, result.ref, result.snapshot as XenApiRecord);
+            recordsStore.addOrReplaceRecord(
+              result.class,
+              result.ref,
+              result.snapshot as XenApiRecord
+            );
           }
         });
       });
@@ -54,10 +60,7 @@ export const useXenApiStore = defineStore('xen-api', () => {
     const hostStore = useHostStore();
     const vmStore = useVmStore();
 
-    await Promise.all([
-      hostStore.init(),
-      vmStore.init(),
-    ]);
+    await Promise.all([hostStore.init(), vmStore.init()]);
 
     const hostMetricsStore = useHostMetricsStore();
     const vmMetricsStore = useVmMetricsStore();
@@ -73,18 +76,23 @@ export const useXenApiStore = defineStore('xen-api', () => {
     consoleStore.init();
   }
 
-  async function connect(login:string, password:string){
-    isConnecting.value = true
+  async function connect(login: string, password: string) {
+    isConnecting.value = true;
     currentSessionId.value = await xenApi.connect(login, password);
-    isConnected.value = true
-    isConnecting.value = false
-    console.log('connected',currentSessionId.value)
+    isConnecting.value = false;
+  }
+
+  function disconnect() {
+    currentSessionId.value = null;
+    xenApi.sessionId = undefined;
+    xenApi.stopWatch();
   }
 
   return {
     isConnected,
     isConnecting,
     connect,
+    disconnect,
     init,
     getXapi,
     currentSessionId,
